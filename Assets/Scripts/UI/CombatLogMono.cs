a07d5607be47e6bd9dec05f640ac840b3efc1071
David Latham <dlatham@live.com>
2021-03-05 22:18:49 -0600

    basic combat log

--- Assets/Scripts/UI/CombatLogMono.cs
+++ Assets/Scripts/UI/CombatLogMono.cs
@@ -8,6 +8,13 @@ using UnityEngine.UI;
 public class CombatLogMono : MonoBehaviour
 {
     public GameObject CombatLog;
+    public TextMeshProUGUI CombatLogContent;
+    public int MaxLineCount;
+
+    private List<string> m_EventLog = new List<string>();
+    private List<string> m_BacklogEvents = new List<string>();
+
+    private bool m_ProcessingEvent = false;
 
     void Start()
     {
@@ -16,16 +23,43 @@ public class CombatLogMono : MonoBehaviour
 
     void LogMessage(string message)
     {
-        
+        if (!m_ProcessingEvent)
+        {
+            m_ProcessingEvent = true;
+            m_EventLog.Add(message);
+            RefreshLog();
+            m_ProcessingEvent = false;
+            if(m_BacklogEvents.Count > 0)
+            {
+                string s = m_BacklogEvents[0];
+                m_BacklogEvents.RemoveAt(0);
+                LogMessage(s);
+            }
+        }
+        else
+        {
+            m_BacklogEvents.Add(message);
+        }
     }
 
-    int m_DebugIndex = 0;
+    void RefreshLog()
+    {
+        while (m_EventLog.Count > MaxLineCount)
+            m_EventLog.RemoveAt(0);
+
+        string guiString = string.Empty;
+        foreach (string log in m_EventLog)
+        {
+            guiString += log;
+            guiString += "\n";
+        }
+
+        CombatLogContent.text = guiString;
+    }
+
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.F1))
             CombatLog.SetActive(!CombatLog.activeSelf);
-
-        if (Input.GetKeyDown(KeyCode.F2))
-            RecLog.Log(m_DebugIndex++);
     }
 }
