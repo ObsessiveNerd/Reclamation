6da882d090ff67deeb8fa24e1282e9f18b7c682c
David Latham <dlatham@live.com>
2022-02-08 19:36:09 -0500

    Moving some shit around

---
+++ Assets/Reclaim/Scripts/UI/IUpdatableUI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IUpdatableUI
+{
+    void UpdateUI(IEntity newSource);
+}
+
+public class UpdatableUI : MonoBehaviour//, IUpdatableUI
+{
+    void Start()
+    {
+        //Services.WorldUIService.RegisterUpdatableUI(this);
+    }
+
+    void OnDisable()
+    {
+        //Services.WorldUIService.UnregisterUpdatableUI(this);
+    }
+
+    public virtual void UpdateUI() { }
+}
